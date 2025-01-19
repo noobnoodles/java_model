7b1842aebbc4d7fce723f71844beecf99a93cc82
NOONES <1404996193@qq.com>
2025-01-20 00:07:14 +0800

    fix bug to start server

---
+++ modules/auth/src/main/java/com/example/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.example.auth;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@MapperScan("com.example.auth.mapper")
+@ComponentScan({"com.example.auth", "com.example.common"})
+public class AuthApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+} 
