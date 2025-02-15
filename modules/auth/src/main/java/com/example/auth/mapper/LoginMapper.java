ff86f9ab9393a1b02436ebce1102823ad66aaff7
NOONES <1404996193@qq.com>
2025-02-15 21:04:34 +0800

    Token send & build finished

--- modules/auth/src/main/java/com/example/auth/mapper/LoginMapper.java
+++ modules/auth/src/main/java/com/example/auth/mapper/LoginMapper.java
@@ -2,20 +2,33 @@ package com.example.auth.mapper;
 
 import com.example.auth.model.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface LoginMapper {
     
     @Select("SELECT * FROM user_info WHERE account = #{account} AND sys_belone = #{sysBelone}")
-    User getUserByAccount(String account, String sysBelone);
+    User getUserByAccount(
+        @Param("account") String account, 
+        @Param("sysBelone") String sysBelone
+    );
     
     @Select("SELECT * FROM user_info WHERE phone = #{phone} AND sys_belone = #{sysBelone}")
-    User getUserByPhone(String phone, String sysBelone);
+    User getUserByPhone(
+        @Param("phone") String phone, 
+        @Param("sysBelone") String sysBelone
+    );
     
     @Select("SELECT * FROM user_info WHERE email = #{email} AND sys_belone = #{sysBelone}")
-    User getUserByEmail(String email, String sysBelone);
+    User getUserByEmail(
+        @Param("email") String email, 
+        @Param("sysBelone") String sysBelone
+    );
 
-    @Select("SELECT * FROM user_info WHERE username = #{account} AND sys_belone = #{sysBelone}")
-    User getUserByUserName(String account, String sysBelone);
+    @Select("SELECT * FROM user_info WHERE username = #{userName} AND sys_belone = #{sysBelone}")
+    User getUserByUserName(
+        @Param("userName") String userName, 
+        @Param("sysBelone") String sysBelone
+    );
 }
