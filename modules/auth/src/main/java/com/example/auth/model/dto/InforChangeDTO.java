69aa23b6a112d27763b024e28f75494e3dbe6e19
NOONES <1404996193@qq.com>
2025-02-19 08:01:17 +0800

    admin control

--- modules/auth/src/main/java/com/example/auth/model/dto/InforChangeDTO.java
+++ modules/auth/src/main/java/com/example/auth/model/dto/InforChangeDTO.java
@@ -1,4 +1,32 @@
 package com.example.auth.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Schema(description = "用户基本数据修改参数")
 public class InforChangeDTO {
+
+    @Schema(description = "头像URL")
+    private String avatar;
+
+    @Schema(description = "性别")
+    private short sexual;
+
+    @Schema(description = "用户名")
+    @NotNull
+    private String userName;
+
+    @Schema(description = "个性签名")
+    private String sign;
+
+    @Schema(description = "账号标识")
+    @NotNull
+    private String accountID;
+
+    @Schema(description = "系统标识")
+    @NotNull
+    private String sysBelone;
+
 }
